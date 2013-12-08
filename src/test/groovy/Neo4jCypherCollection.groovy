d45757772c661abbe5c2bf167b5eb82481f19028
Michael West <quagly@gmail.com>
2013-12-07 16:01:08 -0800

    added Relationship Type test to collection tests

--- src/test/groovy/Neo4jCypherCollection.groovy
+++ src/test/groovy/Neo4jCypherCollection.groovy
@@ -12,8 +12,11 @@ class Neo4jCypherCollection extends spock.lang.Specification {
   @Shared GraphDatabaseService graphDb
   @Shared ExecutionEngine engine
 
-  //instance
-  String cypher   // cypher query string
+  static String staticCypher
+  static QueryStatistics staticQs
+
+  // instance
+  String cypher
   ExecutionResult er
   QueryStatistics qs
   def result = []
@@ -22,30 +25,58 @@ class Neo4jCypherCollection extends spock.lang.Specification {
   def setupSpec() {
     graphDb = new TestGraphDatabaseFactory().newImpermanentDatabaseBuilder().newGraphDatabase()
     engine = new ExecutionEngine( graphDb )
-  }
 
-  def "initialize nodes and relations"() {
-
-  setup: "query to create all nodes and relationships in one statement" 
-    cypher = """ 
-      CREATE (socrates:Philosopher {name:'Socrates', url : 'http://dbpedia.org/resource/Socrates' })
-        , (plato:Philosopher {name:'Plato', url : 'http://dbpedia.org/resource/Plato' })
-        , ( aristotle:Philosopher { name : 'Aristotle' , url : 'http://dbpedia.org/resource/Aristotle' })
+    staticCypher = """ 
+      CREATE
+          ( socrates:Philosopher  {name:'Socrates', uri: 'http://dbpedia.org/resource/Socrates' })
+        , ( plato:Philosopher     {name:'Plato', uri: 'http://dbpedia.org/resource/Plato' })
+        , ( aristotle:Philosopher { name: 'Aristotle' , uri: 'http://dbpedia.org/resource/Aristotle' })
+        , ( ancient_era:Era       { name: 'Ancient philosophy', uri: 'http://dbpedia.org/resource/Ancient_philosophy' })
+        , ( platonism_school:School { name: 'Platonism', uri: 'http://dbpedia.org/resource/Platonism' })
+        , ( peripatetic_school:School { name: 'Peripatetic school', uri: 'http://dbpedia.org/resource/Peripatetic_school' })
+        , ( ancient_greek_school:School { name: 'Ancient Greek philosophy', uri: 'http://dbpedia.org/resource/Ancient_Greek_philosophy' })
+        , ( philo_tradition:SchoolType { name: 'Philosophical traditions', uri: 'http://dbpedia.org/class/yago/PhilosophicalTraditions' })
+        , ( philo_movement:SchoolType { name: 'Philosophical movements', uri: 'http://dbpedia.org/class/yago/PhilosophicalMovements' })
+        , ( philo_ancient_school:SchoolType { name: 'Ancient philosophical schools and traditions', uri: 'http://dbpedia.org/class/yago/AncientPhilosophicalSchoolsAndTraditions' })
+        , ( tradition:SchoolType { name: 'tradition', uri: 'http://dbpedia.org/class/yago/Tradition105809745' })
+        , ( movement:SchoolType { name: 'movement', uri: 'http://dbpedia.org/class/yago/Motion100331950' })
+        , ( school:SchoolType { name: 'school', uri: 'http://dbpedia.org/class/yago/School108276720' })
+        , ( content:SchoolType { name: 'content', uri: 'http://dbpedia.org/class/yago/Content105809192' })
+        , ( knowledge:SchoolType { name: 'knowledge', uri: 'http://dbpedia.org/class/yago/Cognition100023271' })
+        , ( change:SchoolType { name: 'change', uri: 'http://dbpedia.org/class/yago/Change100191142' })
         , socrates-[:INFLUENCES]->plato
         , socrates-[:INFLUENCES]->aristotle
         , plato-[:INFLUENCES]->aristotle
+        , socrates-[:MEMBER_OF]->ancient_greek_school
+        , plato-[:MEMBER_OF]->platonism_school
+        , aristotle-[:MEMBER_OF]->peripatetic_school
+        , socrates-[:MEMBER_OF]->ancient_era
+        , plato-[:MEMBER_OF]->ancient_era
+        , aristotle-[:MEMBER_OF]->ancient_era
+        , platonism_school-[:TYPE_OF]->philo_tradition
+        , platonism_school-[:TYPE_OF]->philo_movement
+        , peripatetic_school-[:TYPE_OF]->philo_movement
+        , peripatetic_school-[:TYPE_OF]->philo_ancient_school
+        , philo_ancient_school-[:SUBCLASS_OF]->school
+        , philo_movement-[:SUBCLASS_OF]->movement
+        , philo_tradition-[:SUBCLASS_OF]->tradition
+        , tradition-[:SUBCLASS_OF]->content
+        , content-[:SUBCLASS_OF]->knowledge
+        , movement-[:SUBCLASS_OF]->change
     """
 
-  when: "execute query and capture stats"
-    er = engine.execute(cypher)
-    qs = er.queryStatistics
+    staticQs = engine.execute(staticCypher).queryStatistics
 
-  then: "validate expected stats"
-    qs.containsUpdates()
-    qs.nodesCreated == 3 
-    qs.relationshipsCreated == 3
-    qs.propertiesSet == 6
-    qs.labelsAdded == 3
+  }
+
+  def "validate setupSpec"() {
+
+  expect: "validate expected stats"
+    staticQs.containsUpdates()
+    staticQs.nodesCreated == 16
+    staticQs.relationshipsCreated == 19
+    staticQs.propertiesSet == 32
+    staticQs.labelsAdded == 16
   }
 
   def "validate nodes"() {
@@ -82,18 +113,43 @@ class Neo4jCypherCollection extends spock.lang.Specification {
   when: "execute query and capture stats"
     er = engine.execute(cypher)
     qs = er.queryStatistics
-    result = er.columnAs("PhilosopherNames").toList() //.sort()
+    result = er.columnAs("PhilosopherNames").toList()
 
   then: "validate expected stats"
     ! qs.containsUpdates()
     // result is a single List of three nodes so size is one array
     // actually it is scala.collection.convert.Wrappers$SeqWrapper
     // instead of List which is why toList() is called below
+    // consider converting result to list of lists in when: 
     result.size() == 1 
     result.first().toList().sort().equals(['Aristotle', 'Plato', 'Socrates'])
 
   }
 
+  def "collect relationships"() {
+
+  setup: "query to collect all Relationship Types from Philosopher Nodes to SchoolType nodes"
+  // store distinct relationship types as a set
+  def relTypes = [] as Set
+  // shows how to collect all RELATIONSHIPS in a path and extract thier names
+    cypher = """
+       MATCH p=(a:Philosopher)-[*]->(b:SchoolType)
+       RETURN DISTINCT EXTRACT( r in RELATIONSHIPS(p)| type(r) ) as RelationshipTypes  
+    """
+
+  when: "execute query and capture stats"
+    er = engine.execute(cypher)
+    qs = er.queryStatistics
+    // result is a list of lists, need to get distinct values from all lists
+    result = er.columnAs("RelationshipTypes").toList()
+    result.each{ relTypes.addAll(it) }
+    //println relTypes.sort()
+
+  then: "validate expected stats"
+    ! qs.containsUpdates()
+    relTypes.size() == 4 
+    relTypes.sort().equals(['INFLUENCES', 'MEMBER_OF', 'SUBCLASS_OF', 'TYPE_OF'])
+  }
 
 }
 
